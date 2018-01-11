43a6dd5351b9da465f184b862e57e5eb1b60bf2c
svenboy@qq.com <svenboy@qq.com>
2018-01-11 13:05:34 +0800

    1

---
+++ src/main/java/com/jx/ticket/GithubdemoApplication.java
@@ -0,0 +1,12 @@
+package com.jx.ticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubdemoApplication.class, args);
+	}
+}
